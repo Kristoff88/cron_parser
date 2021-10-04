a20507a38ddaea9ad9640c285022316313fbda20
Krzysztof Kryspin <ext-lux-kk@ad.datcon.co.uk>
2021-10-04 15:45:52 +0200

    Simple values implemented.

--- src/parse_cron.py
+++ src/parse_cron.py
@@ -5,11 +5,16 @@ class CronStringError(Exception):
 
 def parse_cron(cron_str):
   substrings = [x for x in cron_str.split(' ') if x]
-
-  result = __check_substring_count(substrings)
+  __check_substring_count(substrings)
 
   minute, hour, day_of_month, month, day_of_week = substrings[:5]
   command = " ".join(substrings[5:])
+
+  minute       = __parse_substring(minute,       "Minute",       0, 59)
+  hour         = __parse_substring(hour,         "Hour",         0, 23)
+  day_of_month = __parse_substring(day_of_month, "Day of month", 1, 31)
+  month        = __parse_substring(month,        "Month",        1, 12)
+  day_of_week  = __parse_substring(day_of_week,  "Day of week",  0, 6)
 
   return """
 minute        {}
@@ -29,3 +34,15 @@ def __check_substring_count(substrings):
 
   if num_of_substrings < MINIMAL_SUBSTRINGS_COUNT:
     raise CronStringError("Missing substrings: {}".format(EXPECTED_SUBSTRINGS[num_of_substrings:]))
+
+
+def __parse_substring(substr, name, lower_bound, upper_bound):
+  unwound_expression = substr
+
+  if substr == '*':
+    unwound_expression = ' '.join(str(x) for x in range(lower_bound, upper_bound+1))
+
+  elif int(substr) < lower_bound or int(substr) > upper_bound:
+    raise CronStringError("{} {} outside of range {}-{}!".format(name, int(substr), lower_bound, upper_bound))
+
+  return unwound_expression
--- test/test_parse_cron.py
+++ test/test_parse_cron.py
@@ -17,24 +17,85 @@ def test_given_less_substrings__raises_error():
   assert"Missing substrings: ['day of month', 'month', 'day of week', 'command']" in str(err.value)
 
   with pytest.raises(CronStringError) as err:
-    parse_cron("0 0 0 ")
+    parse_cron("0 0 1 ")
   assert"Missing substrings: ['month', 'day of week', 'command']" in str(err.value)
 
   with pytest.raises(CronStringError) as err:
-    parse_cron("0 0 0 0 ")
+    parse_cron("0 0 1 1 ")
   assert"Missing substrings: ['day of week', 'command']" in str(err.value)
 
   with pytest.raises(CronStringError) as err:
-    parse_cron("0 0 0 0 0 ")
+    parse_cron("0 0 1 1 0 ")
   assert"Missing substrings: ['command']" in str(err.value)
 
 
-def test_given_zeros__returns_zeros():
-  assert parse_cron("0 0 0 0 0 /some_command --with-param; /some_other_command && /yet_another_command") == """
+def test_simple_values__within_range():
+  assert parse_cron("0 0 1 1 0 /some_command") == """
 minute        0
 hour          0
-day of month  0
-month         0
+day of month  1
+month         1
+day of week   0
+command       /some_command
+"""
+
+  assert parse_cron("59 23 31 12 6 /some_command") == """
+minute        59
+hour          23
+day of month  31
+month         12
+day of week   6
+command       /some_command
+"""
+
+
+def test_simple_values__outside_range():
+  with pytest.raises(CronStringError) as err:
+    parse_cron("60 0 1 1 0 /some_command")
+  assert"Minute 60 outside of range 0-59!" in str(err.value)
+
+  with pytest.raises(CronStringError) as err:
+    parse_cron("0 24 1 1 0 /some_command")
+  assert"Hour 24 outside of range 0-23!" in str(err.value)
+
+  with pytest.raises(CronStringError) as err:
+    parse_cron("0 0 0 1 0 /some_command")
+  assert"Day of month 0 outside of range 1-31!" in str(err.value)
+
+  with pytest.raises(CronStringError) as err:
+    parse_cron("0 0 32 1 0 /some_command")
+  assert"Day of month 32 outside of range 1-31!" in str(err.value)
+
+  with pytest.raises(CronStringError) as err:
+    parse_cron("0 0 1 0 0 /some_command")
+  assert"Month 0 outside of range 1-12!" in str(err.value)
+
+  with pytest.raises(CronStringError) as err:
+    parse_cron("0 0 1 13 0 /some_command")
+  assert"Month 13 outside of range 1-12!" in str(err.value)
+
+  with pytest.raises(CronStringError) as err:
+    parse_cron("0 0 1 1 7 /some_command")
+  assert"Day of week 7 outside of range 0-6!" in str(err.value)
+
+
+def test_simple_values__star_expression():
+  assert parse_cron("* * * * * /some_command") == """
+minute        0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59
+hour          0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+day of month  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+month         1 2 3 4 5 6 7 8 9 10 11 12
+day of week   0 1 2 3 4 5 6
+command       /some_command
+"""
+
+
+def test_complex_command():
+  assert parse_cron("0 0 1 1 0 /some_command --with-param; /some_other_command && /yet_another_command") == """
+minute        0
+hour          0
+day of month  1
+month         1
 day of week   0
 command       /some_command --with-param; /some_other_command && /yet_another_command
 """
